fdf7c661342124a7f6e311894e1b7f4bbfec6ad3
RobertMaina <robertmaina254@gmail.com>
2021-10-18 13:17:45 +0300

    add delete todo item functionality

--- src/App.js
+++ src/App.js
@@ -8,12 +8,27 @@ class App extends Component {
       {id: 2, content: "play video games"}
     ]
   };
+  // function to delete todo items is here because it has to interact with 
+  // takes id as parameter so that we can be able to look and and filter from todo array
+  deleteToDo = (id) => {
+    const todos = this.state.todo.filter(todo => {
+      // if todo.id is not eaqual to id param (deleteToDo) return true. If they are equal return false and remove that from the array
+      return todo.id !== id
+    });
+    this.setState({
+      todo: todos 
+    });
+  }
+  // we then pass the function as a prop to todo component so that we can call it when we click on an individual todo 
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h1 className="center blue-text">To Do</h1>
-        <TodoList todo={this.state.todo}/>
+        <TodoList 
+          todo={this.state.todo}
+          deleteToDo={this.deleteToDo}
+        />
       </div>
     )
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const TodoList = ({ todo }) => {
-
+const TodoList = ({ todo, deleteToDo }) => {
     // we need to map because we are getting a list and return jsx for each item
     // terniary operator to return items or text if empty
     const todos = todo.length ? (todo.map(items => {
         return (
             // react expects a unique key on every surrounding element that we return inside map function for easy DOM manipulation
             <div key={items.id} className="collection-item">
-                <span>{items.content}</span>
+                <span onClick={() => deleteToDo(items.id)}>{items.content}</span>
             </div>
-        )
+        );
     })) : (<p className="center">you have no tasks left!</p>)
 
     return (
-        <div className="todolist collection">
+        <div className="collection">
             {todos}
         </div>
-    )
+    );
 }
 
 export default TodoList;
