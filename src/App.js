08bfb780556fddf8891e25403dac70491d6f5722
RobertMaina <robertmaina254@gmail.com>
2021-10-18 12:52:10 +0300

    add components folder with ToDoList.js and output hard coded jsx after mapping through todo props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todo: [
+      {id: 1, content: "buy milk"},
+      {id: 2, content: "play video games"}
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="center blue-text">To Do</h1>
+        <TodoList todo={this.state.todo}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
